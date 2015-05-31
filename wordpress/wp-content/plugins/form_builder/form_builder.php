a57ac6b33b29d61aef08755a7b0ed07b633bbcc2
NourEldin275 <noureldin.ali.m@gmail.com>
2015-05-31 16:56:47 +0200

    testing out some ideas

---
+++ wordpress/wp-content/plugins/form_builder/form_builder.php
@@ -0,0 +1,86 @@
+<?php
+/*
+Plugin Name: Cape East Form Builder
+Plugin URI: http://www.facebook.com
+Description: This plugin builds forms
+Author: Watashi 
+Version: 1.0
+Author URI: http://www.youtube.com
+*/
+	
+
+function form_builder_activator()
+{
+	global $wpdb;
+	$table_name = $wpdb->prefix."formBuilder";
+	if ($wpdb->get_var('SHOW TABLES LIKE '.$table_name) != $table_name)
+	{
+		$sql = 'CREATE TABLE '.$table_name . '(id INTEGER(10) UNSIGNED AUTO_INCREMENT, form_body VARCHAR(255), PRIMARY KEY (id) )';
+		
+		require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+		dbDelta($sql);
+	}	
+}	
+
+register_activation_hook(__FILE__, 'form_builder_activator');
+function form_builder_UI(){
+	?>
+	<h1>Form Builder</h1>
+	<form action="" method="post">
+		<table>
+			<tr>
+				<td><label>User Name</label></td>
+				<td><input type="text" name="username" /></td>
+			</tr>
+			
+			<tr>
+				<td><label>First Name</label></td>
+				<td><input type="text" name="first-name" /></td>
+			</tr>
+			
+			<tr>
+				<td><label>Last Name</label></td>
+				<td><input type="text" name="last-name" /></td>
+			</tr>
+			
+			<tr>
+				<td><label>Email</label></td>
+				<td><input type="email" name="email" /></td>
+			</tr>
+			
+			<tr>
+				<td><button type="submit" name="add-form">Add Form</button></td>
+			</tr>
+		</table>
+	</form>
+	
+	<?php
+}	
+
+function print_form()
+{
+	global $wpdb;
+	$textField = "<input type= \"text\" value=\"Fill in this field\" />";
+	$str = '<input type="text" name="first-name" />';
+	$withAddSlasshesh = addslashes($str);
+	$label = "<label>Name: </label>" ;
+	$form =  $label.$textField.$withAddSlasshesh;
+	echo $form;
+	$wpdb->insert($wpdb->prefix."formBuilder", array('form_body' => $form));
+}
+
+add_shortcode('form-builder', 'print_form');
+function register_frmBuilder()
+{
+		add_menu_page("Form Builder", "Form Builder",'manage_options' ,"form-builder",'form_builder_UI');	
+		//add_options_page("form-builder", "Add new form", 'manage_options', 'form-builder-plugin','form_builder_UI');
+		//add_menu_page("Form Builder", "Form Builder",'manage_options' ,"form-builder");
+}
+	
+	
+
+add_action('admin_menu','register_frmBuilder');
+
+
+
+?>
