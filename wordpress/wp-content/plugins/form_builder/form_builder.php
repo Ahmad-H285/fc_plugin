251c20461a75d4d539c28ab90c2c69192261e2ad
NourEldin275 <noureldin.ali.m@gmail.com>
2015-06-01 14:19:14 +0200

    tested database update

--- wordpress/wp-content/plugins/form_builder/form_builder.php
+++ wordpress/wp-content/plugins/form_builder/form_builder.php
@@ -64,9 +64,9 @@ function print_form()
 	$str = '<input type="text" name="first-name" />';
 	$withAddSlasshesh = addslashes($str);
 	$label = "<label>Name: </label>" ;
-	$form =  $label.$textField.$withAddSlasshesh;
+	$form =  $label.$textField.$withAddSlasshesh.'<div id="eshta"><h1>ESHTA</h1></div>';
 	echo $form;
-	$wpdb->insert($wpdb->prefix."formBuilder", array('form_body' => $form));
+	$wpdb->update($wpdb->prefix."formBuilder", array('form_body' => $form),array('id' => 2));
 }
 
 add_shortcode('form-builder', 'print_form');
