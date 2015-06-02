4bbeb6e7256d0a85e69841d66e6ede6243a06662
Ahmad Hesham <ahmadhesham285@gmail.com>
2015-06-02 10:03:31 +0300

    Submission trials

--- wordpress/wp-content/plugins/form_builder/form_builder.php
+++ wordpress/wp-content/plugins/form_builder/form_builder.php
@@ -62,18 +62,24 @@ function print_form()
 	global $wpdb;
 	$textField = "<input type= \"text\" value=\"Fill in this field\" />";
 	$str = '<input type="text" name="first-name" />';
+	$str1 = '<input type="hidden" name="action" value="form_action250" />';
+	$str2 = '<input type="hidden" name="data" value="foobarid"/>';
 	$withAddSlasshesh = addslashes($str);
 	$label = "<label>Name: </label>" ;
-	
-	$form =  $label.$textField.$withAddSlasshesh;
+	//$sub_but = '<a href="http://localhost:8888/fc_plugin/wordpress/wp-admin/admin-post.php?action=form_action250&data=first-name">Submit</a>';
+	$sub_but = '<input type="submit" value="Submit ya 5ara">';
+	$form = "<form method='POST' action='http://localhost:8888/fc_plugin/wordpress/wp-admin/admin-post.php'>".$label.$textField.$withAddSlasshesh.$sub_but."</form>";
 	$form = htmlentities($form);
 	$wpdb->insert($wpdb->prefix."formBuilder", array('form_body' => $form));
 	$form = html_entity_decode($form);
 	echo $form;
+
 	// $form =  $label.$textField.$withAddSlasshesh.'<div id="eshta"><h1>ESHTA</h1></div>';
 	// echo $form;
 	// $wpdb->update($wpdb->prefix."formBuilder", array('form_body' => $form),array('id' => 2));
 }
+
+add_action('admin_post_form_action250','form_echo');
 
 add_shortcode('form-builder', 'print_form');
 function register_frmBuilder()
@@ -86,6 +92,12 @@ function register_frmBuilder()
 	
 
 add_action('admin_menu','register_frmBuilder');
+
+function form_echo()
+{
+	echo "success";
+	echo $_REQUEST['data'];
+}
 
 
 
