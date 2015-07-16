c1cfcce68ca008177ed9acc46324c721714e739c
Ahmad Hesham <ahmadhesham285@gmail.com>
2015-07-16 12:34:31 +0200

    trying to fix odd elements in edit

--- wordpress/wp-content/plugins/form_builder/js/fcp_js.js
+++ wordpress/wp-content/plugins/form_builder/js/fcp_js.js
@@ -85,7 +85,7 @@ var fcp_date_field = '<div class="form-group"><label for="app_date_'+date_picker
 var fcp_time_field = '<div class="form-group"><label for="app_first_name" class="col-sm-3 control-label">Time</label><div class="col-sm-6 input-container"><input type="number" class="form-control col-sm-3" id="app_first_name" placeholder="hrs" style="width: 70px"><label class="col-sm-1 control-label"> : </label><input type="number" class="form-control col-sm-3" id="app_first_name" placeholder="mins" style="width: 70px"><select class="form-control col-sm-2" style="width:50px; margin-left:15px"><option>AM</option><option>PM</option></select></div><button type="button" class="close" arial-label="Close"><span aria-hidden="true">&times;</span></button><a href="javascript:void(0);" onclick="editFieldOptions(jQuery(this).siblings(&quot;label&quot;).text(),&quot;time&quot;,jQuery(this).parent(),jQuery(this).siblings(&quot;.input-container&quot;).children(&quot;input&quot;).attr(&quot;id&quot;))" class="col-sm-1">Edit</a></div>';
 
 //SELECT MENU
-var fcp_select_field = '<div class="form-group"><label for="app_first_name" class="col-sm-3 control-label">Select Menu</label><div class="col-sm-6 input-container"><select class="form-control"><option>Option 1</option><option>Option 2</option></select></div><button type="button" class="close" arial-label="Close"><span aria-hidden="true">&times;</span></button><a href="javascript:void(0);" onclick="editFieldOptions(jQuery(this).siblings(&quot;label&quot;).text(),&quot;select&quot;,jQuery(this).parent(),jQuery(this).siblings(&quot;.input-container&quot;).children(&quot;input&quot;).attr(&quot;id&quot;))" class="col-sm-1">Edit</a></div>';
+var fcp_select_field = '<div class="form-group"><label for="app_select" class="col-sm-3 control-label">Select Menu</label><div class="col-sm-6 input-container"><select class="form-control" id="app_select"><option>Option 1</option><option>Option 2</option></select></div><button type="button" class="close" arial-label="Close"><span aria-hidden="true">&times;</span></button><a href="javascript:void(0);" onclick="editFieldOptions(jQuery(this).siblings(&quot;label&quot;).text(),&quot;select&quot;,jQuery(this).parent(),jQuery(this).siblings(&quot;.input-container&quot;).children(&quot;select&quot;).attr(&quot;id&quot;))" class="col-sm-1">Edit</a></div>';
 
 //CHECKBOX
 var fcp_checkbox_field = '<div class="check_field"><label class="check_label col-sm-10">Chackbox options</label><a href="javascript:void(0);" onclick="editFieldOptions(jQuery(this).prev(&quot;label&quot;).text(),&quot;checkbox&quot;,jQuery(this).parent())" class="col-sm-1" style="margin-left: 10px;">Edit</a><button type="button" class="close check_close" arial-label=“Close" style="margin-right: -14px;"><span aria-hidden="true">&times;</span></button><div class="form-group"><div class = "checkbox col-sm-10 input-container" style="padding-top:0"><label><input type="checkbox" class="col-sm-4” name=“check"">Checkbox</label></div></div></div>';
@@ -100,7 +100,7 @@ var fcp_email_field = '<div class="form-group"><label for="app_first_name" class
 var fcp_password_field = '<div class="form-group"><label for="app_first_name" class="col-sm-3 control-label">Password</label><div class="col-sm-6 input-container"><input type="password" class="form-control" id="app_first_name" placeholder="Password"></div><button type="button" class="close" arial-label="Close"><span aria-hidden="true">&times;</span></button><a href="javascript:void(0);" onclick="editFieldOptions(jQuery(this).siblings(&quot;label&quot;).text(),jQuery(this).siblings(&quot;div.input-container&quot;).children(&quot;input&quot;).attr(&quot;type&quot;),jQuery(this).parent(),jQuery(this).siblings(&quot;.input-container&quot;).children(&quot;input&quot;).attr(&quot;id&quot;))" class="col-sm-1">Edit</a></div>';
 
 //TEXT AREA
-var fcp_textArea_field = '<div class="form-group"><label for="app_first_name" class="col-sm-3 control-label">Text Area</label><div class="col-sm-6 input-container"><textarea rows="4" cols="50" class="form-control" style="resize: none"></textarea></div><button type="button" class="close" arial-label="Close"><span aria-hidden="true">&times;</span></button><a href="javascript:void(0);" onclick="editFieldOptions(jQuery(this).siblings(&quot;label&quot;).text(),&quot;text-area&quot;,jQuery(this).parent(),jQuery(this).siblings(&quot;.input-container&quot;).children(&quot;textarea&quot;).attr(&quot;id&quot;))" class="col-sm-1">Edit</a></div>'
+var fcp_textArea_field = '<div class="form-group"><label for="app_text_area" class="col-sm-3 control-label">Text Area</label><div class="col-sm-6 input-container"><textarea rows="4" cols="50" class="form-control" style="resize: none" id="app_text_area"></textarea></div><button type="button" class="close" arial-label="Close"><span aria-hidden="true">&times;</span></button><a href="javascript:void(0);" onclick="editFieldOptions(jQuery(this).siblings(&quot;label&quot;).text(),&quot;text-area&quot;,jQuery(this).parent(),jQuery(this).siblings(&quot;.input-container&quot;).children(&quot;textarea&quot;).attr(&quot;id&quot;))" class="col-sm-1">Edit</a></div>'
 
 //file upload
 var fcp_fileSelect_field = '<div class="form-group"><label for="app_attachment" class="col-sm-3 control-label">Attachment</label><div class="col-sm-6 input-container"><input type="file" id="app_attachment"></div><button type="button" class="close" arial-label="Close"><span aria-hidden="true">&times;</span></button><a href="javascript:void(0);" onclick="editFieldOptions(jQuery(this).siblings(&quot;label&quot;).text(),jQuery(this).siblings(&quot;div.input-container&quot;).children(&quot;input&quot;).attr(&quot;type&quot;),jQuery(this).parent(),jQuery(this).siblings(&quot;.input-container&quot;).children(&quot;input&quot;).attr(&quot;id&quot;))" class="col-sm-1">Edit</a></div>';
@@ -227,7 +227,21 @@ jQuery("div#fields-panel button.btn-primary").click(function(){
 		addedField = jQuery("form#fcp_application_preview div.form-group:last").prev();
 	}
 
-	fieldID = jQuery(addedField).children(".input-container").children("input").attr("id");
+	if(jQuery(this).text()== 'Select Menu')
+	{
+		fieldID = jQuery(addedField).children(".input-container").children("select").attr("id");		
+	}
+
+	else if(jQuery(this).text()== 'Text Area')
+	{
+		fieldID = jQuery(addedField).children(".input-container").children("textarea").attr("id");		
+	}
+
+	else
+	{
+		fieldID = jQuery(addedField).children(".input-container").children("input").attr("id");
+	}
+	
 	editFieldOptions(jQuery(this).text(),inputType,addedField,fieldID);
 	fcp_check_deleteField();
 	fcp_radio_deleteField();
@@ -342,6 +356,24 @@ function editFieldOptions(title,type,field,inputID){
 	// next add the options to the div according to their type
 	if (type == "text"){
 		jQuery(name_field_options).prependTo("div#fieldOptions");
+
+		jQuery("input#field-name-option").val(field_name_trim);
+		
+		jQuery("button#saveButton,button#discardButton").click(function(){
+			
+			var field_id = jQuery("input#field-name-option").val()+"_app_"+field_id_num;
+			
+			while(jQuery("input[id='"+field_id+"']").length>0)
+			{
+				field_id_num++;
+				field_id = jQuery("input#field-name-option").val()+"_app_"+field_id_num;
+			}
+			
+			jQuery("input#"+inputID).attr("id",field_id);
+
+			
+
+		});
 	}
 
 	else if (type == "number"){
@@ -352,11 +384,7 @@ function editFieldOptions(title,type,field,inputID){
 		jQuery("input#field-name-option").val(field_name_trim);
 		
 		jQuery("button#saveButton,button#discardButton").click(function(){
-			//if(  !(jQuery("input[id=app_num_field]")) && (jQuery("input[type=number]")))
-			// if( jQuery("input[id='Numeric_app_1']").length>0 )
-			// {
-			// 	field_id_num++;
-			// }
+			
 			var field_id = jQuery("input#field-name-option").val()+"_app_"+field_id_num;
 			
 			while(jQuery("input[id='"+field_id+"']").length>0)
@@ -364,9 +392,7 @@ function editFieldOptions(title,type,field,inputID){
 				field_id_num++;
 				field_id = jQuery("input#field-name-option").val()+"_app_"+field_id_num;
 			}
-			// var id_field_number = 0;
-			//var field_id_catcher = jQuery(field).children("input").attr("id");
-			//var field_id_catcher = jQuery(field).children(".input-container").children("input").attr("id");
+			
 			jQuery("input#"+inputID).attr("id",field_id);
 
 			
@@ -398,22 +424,112 @@ function editFieldOptions(title,type,field,inputID){
 
 	else if (type == "date"){
 		jQuery(name_field_options).prependTo("div#fieldOptions");
+
+		jQuery("input#field-name-option").val(field_name_trim);
+		
+		jQuery("button#saveButton,button#discardButton").click(function(){
+			
+			var field_id = jQuery("input#field-name-option").val()+"_app_"+field_id_num;
+			
+			while(jQuery("input[id='"+field_id+"']").length>0)
+			{
+				field_id_num++;
+				field_id = jQuery("input#field-name-option").val()+"_app_"+field_id_num;
+			}
+			
+			jQuery("input#"+inputID).attr("id",field_id);
+
+			
+
+		});
 	}
 
 	else if (type == "email"){
 		jQuery(name_field_options).prependTo("div#fieldOptions");
+
+		jQuery("input#field-name-option").val(field_name_trim);
+		
+		jQuery("button#saveButton,button#discardButton").click(function(){
+			
+			var field_id = jQuery("input#field-name-option").val()+"_app_"+field_id_num;
+			
+			while(jQuery("input[id='"+field_id+"']").length>0)
+			{
+				field_id_num++;
+				field_id = jQuery("input#field-name-option").val()+"_app_"+field_id_num;
+			}
+			
+			jQuery("input#"+inputID).attr("id",field_id);
+
+			
+
+		});
 	}
 
 	else if (type == "password"){
 		jQuery(name_field_options).prependTo("div#fieldOptions");
+
+		jQuery("input#field-name-option").val(field_name_trim);
+		
+		jQuery("button#saveButton,button#discardButton").click(function(){
+			
+			var field_id = jQuery("input#field-name-option").val()+"_app_"+field_id_num;
+			
+			while(jQuery("input[id='"+field_id+"']").length>0)
+			{
+				field_id_num++;
+				field_id = jQuery("input#field-name-option").val()+"_app_"+field_id_num;
+			}
+			
+			jQuery("input#"+inputID).attr("id",field_id);
+
+			
+
+		});
 	}
 
 	else if (type == "file"){
 		jQuery(name_field_options).prependTo("div#fieldOptions");
+
+		jQuery("input#field-name-option").val(field_name_trim);
+		
+		jQuery("button#saveButton,button#discardButton").click(function(){
+			
+			var field_id = jQuery("input#field-name-option").val()+"_app_"+field_id_num;
+			
+			while(jQuery("input[id='"+field_id+"']").length>0)
+			{
+				field_id_num++;
+				field_id = jQuery("input#field-name-option").val()+"_app_"+field_id_num;
+			}
+			
+			jQuery("input#"+inputID).attr("id",field_id);
+
+			
+
+		});
 	}
 
 	else if (type == "select"){
 		jQuery(name_field_options).prependTo("div#fieldOptions");
+
+		jQuery("input#field-name-option").val(field_name_trim);
+		
+		jQuery("button#saveButton,button#discardButton").click(function(){
+			
+			var field_id = jQuery("input#field-name-option").val()+"_app_"+field_id_num;
+			
+			while(jQuery("select[id='"+field_id+"']").length>0)
+			{
+				field_id_num++;
+				field_id = jQuery("input#field-name-option").val()+"_app_"+field_id_num;
+			}
+			
+			jQuery("select#"+inputID).attr("id",field_id);
+
+			
+
+		});
 	}
 
 	else if (type == "radio"){
@@ -435,10 +551,46 @@ function editFieldOptions(title,type,field,inputID){
 	}
 	else if (type == "time"){
 		jQuery(name_field_options).prependTo("div#fieldOptions");
+
+		jQuery("input#field-name-option").val(field_name_trim);
+		
+		jQuery("button#saveButton,button#discardButton").click(function(){
+			
+			var field_id = jQuery("input#field-name-option").val()+"_app_"+field_id_num;
+			
+			while(jQuery("input[id='"+field_id+"']").length>0)
+			{
+				field_id_num++;
+				field_id = jQuery("input#field-name-option").val()+"_app_"+field_id_num;
+			}
+			
+			jQuery("input#"+inputID).attr("id",field_id);
+
+			
+
+		});
 	}
 
 	else if (type == "text-area"){
 		jQuery(name_field_options).prependTo("div#fieldOptions");
+
+		jQuery("input#field-name-option").val(field_name_trim);
+		
+		jQuery("button#saveButton,button#discardButton").click(function(){
+			
+			var field_id = jQuery("input#field-name-option").val()+"_app_"+field_id_num;
+			
+			while(jQuery("textarea[id='"+field_id+"']").length>0)
+			{
+				field_id_num++;
+				field_id = jQuery("input#field-name-option").val()+"_app_"+field_id_num;
+			}
+			
+			jQuery("textarea#"+inputID).attr("id",field_id);
+
+			
+
+		});
 	}
 
 // appending the required option at the end of the options
