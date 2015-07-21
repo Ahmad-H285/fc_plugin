504f2aa2644075faac6f4d7b59afea1011d52980
NourEldin275 <noureldin.ali.m@gmail.com>
2015-07-21 12:25:21 +0200

    1- Edited the (Edit) link of the predefined inputs so that they ID are passed to the editFieldOptions function. 2- Made the editFieldOptions also update the (for) attribute of the label of the input for text field

--- wordpress/wp-content/plugins/form_builder/forms_UI.php
+++ wordpress/wp-content/plugins/form_builder/forms_UI.php
@@ -8,14 +8,14 @@ function fcp_submissions_page()
 }
 
 function fcp_contact_page()
-{	
+{
 	fcp_stylesheets();
 	?>
 
 	<h1>Contact Form</h1>
-	
+
 	<?php
-	
+
 }
 
 function fcp_survey_page()
@@ -26,7 +26,7 @@ function fcp_survey_page()
 function fcp_application_page()
 {
 	fcp_stylesheets();
-	
+
 	wp_enqueue_script('fcp_js',plugin_dir_url(__FILE__).'js/fcp_js.js', array('jquery','jquery-ui-core','jquery-ui-datepicker'));
 	wp_enqueue_style('jquery-ui-css','http://code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css');
 	wp_enqueue_style('fcp_style.css',plugin_dir_url(__FILE__).'style/fcp_style.css');
@@ -35,11 +35,11 @@ function fcp_application_page()
 			<div class="container-fluid">
 			<div class="row">
 			<h1 class="col-sm-12">Application Form</h1>
-			
-			
+
+
 			<div class="col-sm-7">
 				<form action="" method="POST" class="form-horizontal" id="fcp_application_preview" style="padding: 30px; border: 1px solid #888; box-shadow: 10px 10px 5px #888;">
-					
+
 					<div class="form-group">
 						<label for="app_form_name" class="col-sm-10"><h3>Form Name</h3></label>
 						<div class="col-sm-10">
@@ -48,13 +48,13 @@ function fcp_application_page()
 					</div>
 					<br>
 					<br>
-					  <div class="form-group">  
+					  <div class="form-group">
 					     <label for="app_first_name" class="col-sm-3 control-label ">First Name</label>
 					     <div class="col-sm-6 input-container">
 					      <input type="text" class="form-control" id="app_first_name" placeholder="First Name">
 						 </div>
 						 <button type="button" class="close" arial-label="Close"><span aria-hidden="true">&times;</span></button>
-						 <a href="javascript:void(0);" onclick="editFieldOptions(jQuery(this).siblings(&quot;label&quot;).text(),jQuery(this).siblings(&quot;div.input-container&quot;).children(&quot;input&quot;).attr(&quot;type&quot;),jQuery(this).parent())" class="col-sm-1">Edit</a>
+						 <a href="javascript:void(0);" onclick="editFieldOptions(jQuery(this).siblings(&quot;label&quot;).text(),jQuery(this).siblings(&quot;div.input-container&quot;).children(&quot;input&quot;).attr(&quot;type&quot;),jQuery(this).parent(),jQuery(this).siblings(&quot;.input-container&quot;).children(&quot;input&quot;).attr(&quot;id&quot;))" class="col-sm-1">Edit</a>
 					  </div>
 
 					  <div class="form-group">
@@ -63,7 +63,7 @@ function fcp_application_page()
 					      <input type="text" class="form-control" id="app_last_name" placeholder="Last Name">
 					    </div>
 					    <button type="button" class="close" arial-label="Close"><span aria-hidden="true">&times;</span></button>
-					    <a href="javascript:void(0);" onclick="editFieldOptions(jQuery(this).siblings(&quot;label&quot;).text(),jQuery(this).siblings(&quot;div.input-container&quot;).children(&quot;input&quot;).attr(&quot;type&quot;),jQuery(this).parent())" class="col-sm-1">Edit</a>
+					    <a href="javascript:void(0);" onclick="editFieldOptions(jQuery(this).siblings(&quot;label&quot;).text(),jQuery(this).siblings(&quot;div.input-container&quot;).children(&quot;input&quot;).attr(&quot;type&quot;),jQuery(this).parent(),jQuery(this).siblings(&quot;.input-container&quot;).children(&quot;input&quot;).attr(&quot;id&quot;))" class="col-sm-1">Edit</a>
 					  </div>
 
 					  <div class="form-group">
@@ -72,24 +72,24 @@ function fcp_application_page()
 					      <input type="email" class="form-control" id="app_email" placeholder="Email">
 					    </div>
 					    <button type="button" class="close" arial-label="Close"><span aria-hidden="true">&times;</span></button>
-					    <a href="javascript:void(0);" onclick="editFieldOptions(jQuery(this).siblings(&quot;label&quot;).text(),jQuery(this).siblings(&quot;div.input-container&quot;).children(&quot;input&quot;).attr(&quot;type&quot;),jQuery(this).parent())" class="col-sm-1">Edit</a>
+					    <a href="javascript:void(0);" onclick="editFieldOptions(jQuery(this).siblings(&quot;label&quot;).text(),jQuery(this).siblings(&quot;div.input-container&quot;).children(&quot;input&quot;).attr(&quot;type&quot;),jQuery(this).parent(),jQuery(this).siblings(&quot;.input-container&quot;).children(&quot;input&quot;).attr(&quot;id&quot;))" class="col-sm-1">Edit</a>
 					  </div>
-					  
+
 					  <div class="form-group">
 					    <label for="app_opt" class="col-sm-4 control-label">Application Options</label>
-					    <div class="col-sm-5">
+					    <div class="col-sm-5  input-container">
 					      <select class="form-control" id="app_opt"></select>
 					    </div>
 					    <button type="button" class="close" arial-label="Close"><span aria-hidden="true">&times;</span></button>
-					    <a href="javascript:void(0);" onclick="editFieldOptions(jQuery(this).siblings(&quot;label&quot;).text(),&quot;select&quot;,jQuery(this).parent())" class="col-sm-1">Edit</a>
+					    <a href="javascript:void(0);" onclick="editFieldOptions(jQuery(this).siblings(&quot;label&quot;).text(),&quot;select&quot;,jQuery(this).parent(),jQuery(this).siblings(&quot;.input-container&quot;).children(&quot;select&quot;).attr(&quot;id&quot;))" class="col-sm-1">Edit</a>
 					  </div>
-					  
+
 					  <div class="radio_field">
 					  <label class="radio_label col-sm-9">Gender</label>
 					  <a href="javascript:void(0);" onclick="editFieldOptions(jQuery(this).prev(&quot;label&quot;).text(),&quot;radio&quot;,jQuery(this).parent())" class="col-sm-1" style="margin-left: 10px;">Edit</a>
 					  <button type="button" class="close radio_close" arial-label="Close" style="margin-right: -14px;"><span aria-hidden="true">&times;</span></button>
 					  <div class="form-group">
-						
+
 					  	<div class = "radio col-sm-10 input-container" style="padding-top:0">
 							<label><input name="gender" type="radio" class="col-sm-4">Male</label>
 						</div>
@@ -119,11 +119,11 @@ function fcp_application_page()
 			</div></div><div class="row" style="padding: 20px"><button type="button" class="btn btn-danger">Save Form</button></div></div>';
 			$app_form = $fcp_default_app_form . $fcp_app_form_close;
 
-			echo $app_form;			
+			echo $app_form;
 
 			fcp_fields_panel();
 			fcp_fields_options();
-	
+
 }
 
 function fcp_registration_page()
--- wordpress/wp-content/plugins/form_builder/js/fcp_js.js
+++ wordpress/wp-content/plugins/form_builder/js/fcp_js.js
@@ -33,7 +33,7 @@ function fcp_check_deleteField(){
 		jQuery(this).parent().remove();
 		jQuery("div#edit_field_content").removeClass("show").addClass("hidden");
 		jQuery("div#edit_field_title").removeClass("show").addClass("hidden");
-	});	
+	});
 
 }
 
@@ -43,15 +43,15 @@ function fcp_radio_deleteField(){
 		jQuery(this).parent().remove();
 		jQuery("div#edit_field_content").removeClass("show").addClass("hidden");
 		jQuery("div#edit_field_title").removeClass("show").addClass("hidden");
-	});	
+	});
 
 }
 
  // function fcp_numeric_range_activator(event){
-	
+
  // 		var max_length_field = jQuery("input#num-max").val();
  // 		event.data.attr("max", max_length_field);
-	
+
  // }
 
 // the following function handles making a field required or not
@@ -157,7 +157,7 @@ jQuery("div#fields-panel button.btn-primary").click(function(){
 	var fieldID;
 
 	if(jQuery(this).text() == 'Text'){
-		
+
 		jQuery("form#fcp_application_preview div.form-group:last").before(fcp_text_field);
 		inputType = "text";
 	}
@@ -219,7 +219,7 @@ jQuery("div#fields-panel button.btn-primary").click(function(){
 		addedField = jQuery("div.radio_field:last");
 	}
 
-	else if( jQuery(this).text() == 'Checkbox' ){ // special case for checkbox 
+	else if( jQuery(this).text() == 'Checkbox' ){ // special case for checkbox
 		addedField = jQuery("div.check_field:last");
 	}
 
@@ -229,19 +229,19 @@ jQuery("div#fields-panel button.btn-primary").click(function(){
 
 	if(jQuery(this).text()== 'Select Menu')
 	{
-		fieldID = jQuery(addedField).children(".input-container").children("select").attr("id");		
+		fieldID = jQuery(addedField).children(".input-container").children("select").attr("id");
 	}
 
 	else if(jQuery(this).text()== 'Text Area')
 	{
-		fieldID = jQuery(addedField).children(".input-container").children("textarea").attr("id");		
+		fieldID = jQuery(addedField).children(".input-container").children("textarea").attr("id");
 	}
 
 	else
 	{
 		fieldID = jQuery(addedField).children(".input-container").children("input").attr("id");
 	}
-	
+
 	editFieldOptions(jQuery(this).text(),inputType,addedField,fieldID);
 	fcp_check_deleteField();
 	fcp_radio_deleteField();
@@ -268,7 +268,7 @@ function discardChanges(event){
 */
 
 	jQuery("div#edit_field_title").toggleClass("show").addClass("hidden");
-	jQuery("div#edit_field_content").toggleClass("show").addClass("hidden");	
+	jQuery("div#edit_field_content").toggleClass("show").addClass("hidden");
 	jQuery("button#discardButton").off("click");
 	jQuery("input#range_active").off("click");
 	jQuery("input#required-option").off("click");
@@ -296,7 +296,7 @@ jQuery("button#saveButton").click(function(){
 // function lambaz(event)
 // {
 // 	var field_id = jQuery("input#field-name-option").val()+"_app_"+field_id_num;
-			
+
 // 			while(jQuery("input[id='"+field_id+"']").length>0)
 // 			{
 // 				field_id_num++;
@@ -322,10 +322,10 @@ function updateFieldLabel(event){ // field label is in the event.data.label obje
 	}
 
 	else{
-		jQuery(event.data.label).text(jQuery(this).val());	
+		jQuery(event.data.label).text(jQuery(this).val());
 	}
 
-	
+
 }
 
 /******************* updateFieldLabel End *******************/
@@ -339,17 +339,18 @@ function updateFieldLabel(event){ // field label is in the event.data.label obje
  *	(2) the value of the "type" attribute of the input or simply text containing the type of the input for special case inputs (select tag)
  *	(3) the jQuery object of the entire parent "div" tag which has "form-group" as a class
  * 	For example:
- *	
+ *
  *  	editFieldOptions('First Name','text',jQuery("div.form-group:first"));
  */
  var testing;
-function editFieldOptions(title,type,field,inputID){ 
-	
+function editFieldOptions(title,type,field,inputID){
+
 	jQuery("div#edit_field_content").removeClass("hidden").addClass("show");
 	jQuery("div#edit_field_title").removeClass("hidden").addClass("show").html('<h4> Edit '+title+' Field</h4>');
 	jQuery("div#fieldOptions").empty(); // to remove other fields options before displaying other fields options
 	var field_values = {label: title.replace('*','')}; // used the replace function to remove the required mark if it exists
 	var field_id_num = 1;
+	var field_Id_NoSpaces; // variable to hold the id after removing spaces
 	//console.log(field_values);
 	//before_edit_label = title;
 	var field_name_trim = jQuery.trim(jQuery("div#edit_field_title").text().split('Edit')[1].split('Field')[0]);
@@ -358,20 +359,20 @@ function editFieldOptions(title,type,field,inputID){
 		jQuery(name_field_options).prependTo("div#fieldOptions");
 
 		jQuery("input#field-name-option").val(field_name_trim);
-		
+
 		jQuery("button#saveButton,button#discardButton").click(function(){
-			
+
 			var field_id = jQuery("input#field-name-option").val()+"_app_"+field_id_num;
-			
+
 			while(jQuery("input[id='"+field_id+"']").length>0)
 			{
 				field_id_num++;
 				field_id = jQuery("input#field-name-option").val()+"_app_"+field_id_num;
 			}
-			
-			jQuery("input#"+inputID).attr("id",field_id.replace(/\s+/g, ''));
+			field_Id_NoSpaces = field_id.replace(/\s+/g, ''); // removing the spaces from the id
+			jQuery("input#"+inputID).attr("id",field_Id_NoSpaces).parent(".input-container").prev("label").attr("for",field_Id_NoSpaces);
 
-			
+
 
 		});
 	}
@@ -382,20 +383,20 @@ function editFieldOptions(title,type,field,inputID){
 		jQuery(max_range_field_options).appendTo("div#fieldOptions");
 
 		jQuery("input#field-name-option").val(field_name_trim);
-		
+
 		jQuery("button#saveButton,button#discardButton").click(function(){
-			
+
 			var field_id = jQuery("input#field-name-option").val()+"_app_"+field_id_num;
-			
+
 			while(jQuery("input[id='"+field_id+"']").length>0)
 			{
 				field_id_num++;
 				field_id = jQuery("input#field-name-option").val()+"_app_"+field_id_num;
 			}
-			
+
 			jQuery("input#"+inputID).attr("id",field_id.replace(/\s+/g, ''));
 
-			
+
 
 		});
 		// jQuery("input#range_active").click(function(field){
@@ -409,37 +410,37 @@ function editFieldOptions(title,type,field,inputID){
 		// 	{
 		// 		jQuery("input#num-max").attr("disabled","true");
 		// 	}
-			
+
 		// });
 
 		// jQuery("input#num-max").blur(function(field){
 			// jQuery("button#saveButton").click(function(field){
 
 			// 	fcp_numeric_range_activator(jQuery("div.form-group").children(".input-container").children("input#app_num_field"));
-			
+
 			// });
-			
-		
+
+
 	}
 
 	else if (type == "date"){
 		jQuery(name_field_options).prependTo("div#fieldOptions");
 
 		jQuery("input#field-name-option").val(field_name_trim);
-		
+
 		jQuery("button#saveButton,button#discardButton").click(function(){
-			
+
 			var field_id = jQuery("input#field-name-option").val()+"_app_"+field_id_num;
-			
+
 			while(jQuery("input[id='"+field_id+"']").length>0)
 			{
 				field_id_num++;
 				field_id = jQuery("input#field-name-option").val()+"_app_"+field_id_num;
 			}
-			
+
 			jQuery("input#"+inputID).attr("id",field_id.replace(/\s+/g, ''));
 
-			
+
 
 		});
 	}
@@ -448,20 +449,20 @@ function editFieldOptions(title,type,field,inputID){
 		jQuery(name_field_options).prependTo("div#fieldOptions");
 
 		jQuery("input#field-name-option").val(field_name_trim);
-		
+
 		jQuery("button#saveButton,button#discardButton").click(function(){
-			
+
 			var field_id = jQuery("input#field-name-option").val()+"_app_"+field_id_num;
-			
+
 			while(jQuery("input[id='"+field_id+"']").length>0)
 			{
 				field_id_num++;
 				field_id = jQuery("input#field-name-option").val()+"_app_"+field_id_num;
 			}
-			
+
 			jQuery("input#"+inputID).attr("id",field_id.replace(/\s+/g, ''));
 
-			
+
 
 		});
 	}
@@ -470,20 +471,20 @@ function editFieldOptions(title,type,field,inputID){
 		jQuery(name_field_options).prependTo("div#fieldOptions");
 
 		jQuery("input#field-name-option").val(field_name_trim);
-		
+
 		jQuery("button#saveButton,button#discardButton").click(function(){
-			
+
 			var field_id = jQuery("input#field-name-option").val()+"_app_"+field_id_num;
-			
+
 			while(jQuery("input[id='"+field_id+"']").length>0)
 			{
 				field_id_num++;
 				field_id = jQuery("input#field-name-option").val()+"_app_"+field_id_num;
 			}
-			
+
 			jQuery("input#"+inputID).attr("id",field_id.replace(/\s+/g, ''));
 
-			
+
 
 		});
 	}
@@ -492,20 +493,20 @@ function editFieldOptions(title,type,field,inputID){
 		jQuery(name_field_options).prependTo("div#fieldOptions");
 
 		jQuery("input#field-name-option").val(field_name_trim);
-		
+
 		jQuery("button#saveButton,button#discardButton").click(function(){
-			
+
 			var field_id = jQuery("input#field-name-option").val()+"_app_"+field_id_num;
-			
+
 			while(jQuery("input[id='"+field_id+"']").length>0)
 			{
 				field_id_num++;
 				field_id = jQuery("input#field-name-option").val()+"_app_"+field_id_num;
 			}
-			
+
 			jQuery("input#"+inputID).attr("id",field_id.replace(/\s+/g, ''));
 
-			
+
 
 		});
 	}
@@ -514,20 +515,20 @@ function editFieldOptions(title,type,field,inputID){
 		jQuery(name_field_options).prependTo("div#fieldOptions");
 
 		jQuery("input#field-name-option").val(field_name_trim);
-		
+
 		jQuery("button#saveButton,button#discardButton").click(function(){
-			
+
 			var field_id = jQuery("input#field-name-option").val()+"_app_"+field_id_num;
-			
+
 			while(jQuery("select[id='"+field_id+"']").length>0)
 			{
 				field_id_num++;
 				field_id = jQuery("input#field-name-option").val()+"_app_"+field_id_num;
 			}
-			
+
 			jQuery("select#"+inputID).attr("id",field_id.replace(/\s+/g, ''));
 
-			
+
 
 		});
 	}
@@ -553,20 +554,20 @@ function editFieldOptions(title,type,field,inputID){
 		jQuery(name_field_options).prependTo("div#fieldOptions");
 
 		jQuery("input#field-name-option").val(field_name_trim);
-		
+
 		jQuery("button#saveButton,button#discardButton").click(function(){
-			
+
 			var field_id = jQuery("input#field-name-option").val()+"_app_"+field_id_num;
-			
+
 			while(jQuery("input[id='"+field_id+"']").length>0)
 			{
 				field_id_num++;
 				field_id = jQuery("input#field-name-option").val()+"_app_"+field_id_num;
 			}
-			
+
 			jQuery("input#"+inputID).attr("id",field_id.replace(/\s+/g, ''));
 
-			
+
 
 		});
 	}
@@ -575,20 +576,20 @@ function editFieldOptions(title,type,field,inputID){
 		jQuery(name_field_options).prependTo("div#fieldOptions");
 
 		jQuery("input#field-name-option").val(field_name_trim);
-		
+
 		jQuery("button#saveButton,button#discardButton").click(function(){
-			
+
 			var field_id = jQuery("input#field-name-option").val()+"_app_"+field_id_num;
-			
+
 			while(jQuery("textarea[id='"+field_id+"']").length>0)
 			{
 				field_id_num++;
 				field_id = jQuery("input#field-name-option").val()+"_app_"+field_id_num;
 			}
-			
+
 			jQuery("textarea#"+inputID).attr("id",field_id.replace(/\s+/g, ''));
 
-			
+
 
 		});
 	}
@@ -613,7 +614,7 @@ jQuery("input#required-option").click({element: field.children("label")},require
 	jQuery("input#field-name-option").keyup({ label: field.children("label")},updateFieldLabel);
 
 	//the following line triggers the click event on the "discardButton"
-	//it will invoke the function (discardCHanges) and passes it data in the form of three objects: 
+	//it will invoke the function (discardCHanges) and passes it data in the form of three objects:
 	// 1 The options which will be created in an object
 	// 2 The Field parent (div.form-group) to allow traversing
 	// 3 The Field Type to do special case logic
@@ -621,4 +622,3 @@ jQuery("input#required-option").click({element: field.children("label")},require
 	jQuery("button#discardButton").click({field_values, element: field, fieldType: type},discardChanges);
 
 }
-
