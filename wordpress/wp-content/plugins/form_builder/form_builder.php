4c1ca7e955b5c028528efb9c9b9ff9729b1e3198
Ahmad Hesham <ahmadhesham285@gmail.com>
2015-11-01 13:33:10 +0200

    added user guide referral

--- wordpress/wp-content/plugins/form_builder/form_builder.php
+++ wordpress/wp-content/plugins/form_builder/form_builder.php
@@ -290,7 +290,7 @@ function fcp_general_page()
     </div>
     <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
       <div class="panel-body">
-        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+        FCP plugin is a form builder plugin that can be used to build multiple form types with unique settings. To learn how to use it, please visit our <a href="http://cape-east.co/fcp-plugin-user-guide/">User Guide</a> page.
       </div>
     </div>
   </div>
